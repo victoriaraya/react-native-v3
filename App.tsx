cd0e0b4b70b818d914686f637f6efaf6e7987288
Victoria Raya <victoriaraya111@gmail.com>
2025-01-18 22:19:02 -0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>An app to track the phases of the moon</Text>
       <StatusBar style="auto" />
     </View>
   );
