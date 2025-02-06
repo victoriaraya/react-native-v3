d66441439e550648b9b60fdabde68b59b0fa5488
Victoria Raya <victoriaraya111@gmail.com>
2025-02-05 23:34:01 -0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>taskly </Text>
+      <Text>taskly</Text>
       <StatusBar style="auto" />
     </View>
   );
